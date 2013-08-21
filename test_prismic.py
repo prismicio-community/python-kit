49da3c2cb156e947c9d81a6a6aa2cbfab16eef11
Nicolae Namolovan <nna@zenexity.com>
2013-08-21 18:16:37 +0200

    More tests, test fixtures moved to a separate file.

--- test_prismic.py
+++ test_prismic.py
@@ -9,86 +9,63 @@ import unittest
 import json
 from prismic.exceptions import (InvalidTokenError,
                                 AuthorizationNeededError, UnexpectedError)
-
-api_sample_data = """
-{
-    "refs": [{
-        "ref": "UgjWQN_mqa8HvPJY",
-        "label": "Master",
-        "isMasterRef": true
-    }, {
-        "ref": "UgjWRd_mqbYHvPJa",
-        "label": "San Francisco Grand opening"
-    }],
-    "bookmarks": {
-        "about": "Ue0EDd_mqb8Dhk3j",
-        "jobs": "Ue0EHN_mqbwDhk3l",
-        "stores": "Ue0EVt_mqd8Dhk3n"
-    },
-    "types": {
-        "blog-post": "Blog post",
-        "store": "Store",
-        "article": "Site-level article",
-        "selection": "Products selection",
-        "job-offer": "Job offer",
-        "product": "Product"
-    },
-    "tags": ["Cupcake", "Pie", "Featured", "Macaron"],
-    "forms": {
-        "everything": {
-            "method": "GET",
-            "enctype": "application/x-www-form-urlencoded",
-            "action": "http://lesbonneschoses.wroom.io/api/documents/search",
-            "fields": {
-                "ref": {
-                    "type": "String"
-                },
-                "q": {
-                    "type": "String"
-                }
-            }
-        }
-    },
-    "oauth_initiate": "http://lesbonneschoses.wroom.io/auth",
-    "oauth_token": "http://lesbonneschoses.wroom.io/auth/token"
-}"""
+from test_prismic_fixtures import api_sample_data, search_sample_data
 
 class PrismicTestCase(unittest.TestCase):
     def setUp(self):
         """Init the api url and the token identifier."""
-        self.api = "http://lesbonneschoses.wroom.io/api"
+        self.api_url = "http://lesbonneschoses.wroom.io/api"
         self.token = "MC5VZ2phOGRfbXFaOEl2UEpj.dO-_ve-_ve-_ve-_vSFRBzXvv71V77-977-9BO-_vVbvv71k77-9Cu-_ve-_vQTvv71177-9eQpcUE3vv70"
-        self.api_sample_data = json.loads(api_sample_data)
-        self.refLabel = "Master"
+        self.api_fixture_data = json.loads(api_sample_data)
+        self.search_fixture_data = json.loads(search_sample_data)
+
+        self.api = prismic.Api(self.api_fixture_data, self.token)
 
     def tearDown(self):
         """Teardown."""
 
 
-class ApiTestCase(PrismicTestCase):
+class ApiIntegrationTestCase(PrismicTestCase):
+    """Doing real HTTP requests to test API data fetching"""
 
     def test_get_api(self):
-        api = prismic.get(self.api, self.token)
+        api = prismic.get(self.api_url, self.token)
         self.assertTrue(len(api.forms) > 0)
 
     def test_api_get_errors(self):
         with self.assertRaises(InvalidTokenError):
-            prismic.get(self.api, "wrong")
+            prismic.get(self.api_url, "wrong")
 
         with self.assertRaises(AuthorizationNeededError):
-            prismic.get(self.api, "")
+            prismic.get(self.api_url, "")
 
         with self.assertRaises(UnexpectedError):
             prismic.get("htt://wrong_on_purpose", "")
 
 
+class ApiTestCase(PrismicTestCase):
+    def test_get_ref(self):
+        self.assertTrue(self.api.get_ref("Master").ref == "UgjWQN_mqa8HvPJY")
+
+
 class TestSearchFormTestCase(PrismicTestCase):
-    def test_search_form(self):
-        api = prismic.Api(self.api_sample_data, self.token)
-        everything = api.get_form("everything")
-        everything.ref_by_label("Master")
-        result = everything.submit()
-        print(result)
+    #def test_search_form(self):
+    #    everything = self.api.form("everything")
+    #    everything.ref("Master")
+    #    docs = everything.submit()
+    #    self.assertTrue(len(docs) == 20)
+
+    def test_document(self):
+        docs = [prismic.Document(doc) for doc in self.search_fixture_data]
+        self.assertTrue(len(docs) == 3)
+        doc = docs[0]
+        self.assertTrue(doc.slug == "vanilla-macaron")
+
+    def test_empty_slug(self):
+        doc_json = self.search_fixture_data[0]
+        doc_json["slugs"] = None
+        doc = prismic.Document(doc_json)
+        self.assertTrue(doc.slug == "-")
 
 
 if __name__ == '__main__':
