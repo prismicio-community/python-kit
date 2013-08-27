4273560c7c22147f80afad127fb34e37927f21e6
Nicolae Namolovan <nna@zenexity.com>
2013-08-27 18:23:10 +0200

    Handle the logging properly.

--- prismic/__init__.py
+++ prismic/__init__.py
@@ -9,3 +9,14 @@ __license__ = ''
 
 from .api import get, Api, SearchForm, Document
 import structured_text
+
+# Set a default logger to prevent "No handler found" warnings
+import logging
+try:  # Python >=2.7
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
