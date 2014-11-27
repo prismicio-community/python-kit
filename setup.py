b536848c5b18ecab955019df03f4e8c264a68db1
Erwan Loisant <eloisant@gmail.com>
2014-11-28 00:09:46 +0100

    v1.0.13

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 
 setup(
     name='prismic',
-    version='1.0.12',
+    version='1.0.13',
     description='Prismic.io development kit',
     author='The Prismic.io Team',
     author_email='contact@prismic.io',
