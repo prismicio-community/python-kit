16f33ecc43aac7987fc62478942cec28ba2427d9
Zelane <zelane@users.noreply.github.com>
2017-10-11 21:23:06 +0100

    Upgraded the minor version to 1.5.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 
 setup(
     name='prismic',
-    version='1.4.3',
+    version='1.5.0',
     description='Prismic.io development kit',
     author='The Prismic.io Team',
     author_email='contact@prismic.io',
